a9529a32617e37248d09f20c3b8593eb3c119ebb
Lydia Athanasiou <71638749+lydia-ath@users.noreply.github.com>
2022-03-10 20:59:53 +0200

    project 2

---
+++ 4rth semester/operating_systems/2nd assignment/header.h
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+
+const unsigned int Ntel = 8;
+const unsigned int Ncash = 4;
+const unsigned int Nseat = 10;
+const unsigned int NzoneA = 5;
+const unsigned int NzoneB = 10;
+const unsigned int NzoneC = 10;
+const unsigned int PzoneA = 20;
+const unsigned int PzoneB = 40;
+const unsigned int PzoneC = 40;
+const unsigned int CzoneA = 30;
+const unsigned int CzoneB = 25;
+const unsigned int CzoneC = 20;
+const unsigned int Nseatlow = 1;
+const unsigned int Nseathigh = 5;
+const unsigned int Tseatlow = 5;
+const unsigned int Tseathigh = 10;
+const unsigned int Tcashlow = 2;
+const unsigned int Tcashhigh = 4;
+const double Pcardsuccess = 90;
